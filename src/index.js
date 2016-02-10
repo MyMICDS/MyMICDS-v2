a46fe566c422cf4c123906d2c6492931ec18e9c5
Michael <michaelgira23@gmail.com>
2016-02-10 10:27:32 -0600

    Importing Some more Libraries

--- src/index.js
+++ src/index.js
@@ -11,14 +11,25 @@ var credentials =
 };
 */
 
-/* Frameworks */
+/* Express */
 
 var express = require('express');
 var app = express();
 var server = http.Server(app);
-// server = https.Server(app);
 
 var io = require('socket.io')(server);
+
+/* Session */
+
+/* 
+ * IMPORTANT: Do not use the default MemoryStore session store. It is not designed for a production environment.
+ * If you do use the default store then you should pass the same session instance to express and socket app (data are saved in memory).
+ * https://www.npmjs.com/package/connect-mongo
+ */
+
+var ios = require('socket.io-express-session');
+// TODO: Configure express session (https://github.com/expressjs/session#sessionoptions)
+var session = require('express-session');
 
 var port = 420;
 server.listen(port, function() {
@@ -27,4 +38,8 @@ server.listen(port, function() {
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/html/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('Connection');
 });
