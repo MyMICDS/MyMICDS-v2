cf5a24c7aca026267a5f897b8715da98e187f4b3
Ccoolboy <michaelgira23@gmail.com>
2016-02-11 23:43:41 -0700

    Try to get MongoDB connection up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 var port = 420;
 
+/* I am a Mon-god */
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// Connection URL
+/*var url = 'mongodb://45.56.70.141';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    
+    db.close();
+});*/
+
 /* General Libraries */
 
 var bodyParser = require('body-parser')
