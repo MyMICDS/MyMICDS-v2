e0a43c8f538a670d626c2b5fb704f44fcd49002f
AsianPsychoBoy <ziyang_99@outlook.com>
2016-02-19 13:10:31 -0600

    prints the lunchobject on the page

--- src/lunch.js
+++ src/lunch.js
@@ -1,4 +1,23 @@
 var Lunch = require('./getLunch.js');
 var getLunch = new Lunch;
 
-getLunch.on("end", console.dir);
+var lunchObject = {};
+getLunch.on("end", function (l) {
+	lunchObject = l;
+});
+getLunch.on("error", function (err) {
+	console.dir(err);
+});
+
+var express = require('express');
+lunchApp = express();
+
+lunchApp.get("/lunch", function (req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.send(lunchObject);
+	res.end;
+});
+
+lunchApp.listen("3000", function () {
+	console.log('Success')
+});
