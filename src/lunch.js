1b6719857108a9162e6f94e93f1e802bcea016b0
AsianPsychoBoy <ziyang_99@outlook.com>
2016-02-19 19:29:10 -0600

    i dont know y its not displaying html

--- src/lunch.js
+++ src/lunch.js
@@ -1,7 +1,16 @@
-var Lunch = require('./getLunch.js');
-var getLunch = new Lunch;
+'use strict';
 
+var Lunch = require('./getLunch.js');
+var ejs = require('ejs')
+var getLunch = new Lunch;
 var lunchObject = {};
+var express = require('express');
+var lunchApp = express();
+
+var templateString = null;
+var fs = require('fs');
+var templateString = fs.readFileSync('./html/lunch.ejs', 'utf-8');
+
 getLunch.on("end", function (l) {
 	lunchObject = l;
 });
@@ -9,15 +18,19 @@ getLunch.on("error", function (err) {
 	console.dir(err);
 });
 
-var express = require('express');
-lunchApp = express();
+delete lunchObject.info;
 
+//prepairing the lunch template with ejs.
+lunchApp.set('view engine', 'ejs');
+
+var lunchHTML = ejs.render(templateString, {lunchObject: lunchObject});
+
+//Routing
 lunchApp.get("/lunch", function (req, res) {
-	res.setHeader('Content-Type', 'text/plain');
-	res.send(lunchObject);
-	res.end;
+	res.setHeader('Content-Type', 'text/html');
+	if (typeof lunchHTML != "undefined") {res.send(lunchHTML);} else {res.send('<h1>undefined</h1>')};
 });
 
 lunchApp.listen("3000", function () {
-	console.log('Success')
+	console.log('listening on port 3000')
 });
