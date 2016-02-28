9ff1a27f9f88d439b064cfa74b2696548b34b564
Michael <michaelgira23@gmail.com>
2016-02-28 04:34:29 +0000

    automated sass watching

---
+++ src/libs/sass.js
@@ -0,0 +1,45 @@
+/**
+ * @file Compiles Sass
+ * @module sass
+ */
+
+// libs
+var fs   = require('fs');
+var sass = require('node-sass');
+
+/**
+ * Watches directory for changes and compiles Sass.
+ * @function watchSass
+ * 
+ * @param {string} sassDir Directory in which the Sass files are kept.
+ * @param {string} cssDir Directory where the compiled CSS will be written.
+ */
+
+function watchSass(sassDir, cssDir) {
+    fs.watch(sassDir, function(event, filename) {
+        var sassFile = sassDir + '/' + filename;
+        sass.render({
+            file: sassFile,
+        }, function(sassErr, result) {
+            if(!sassErr) {
+                // Gets raw filename sans extension
+                var cssFilename = filename.substring(0, filename.indexOf('.'));
+                // Appends .css file extension
+                cssFilename += '.css';
+                
+                var cssFile = cssDir + '/' + cssFilename;
+                fs.writeFile(cssFile, result.css, function(writeErr) {
+                    if(!writeErr) {
+                        console.log('File ' + sassFile + ' was compiled to ' + cssFile);
+                    } else {
+                        console.log('There was a problem writing the file! Check to make sure node has proper write permissions.');
+                    }
+                });
+            } else {
+                console.log('Sass error: ' + sassErr.message + ' in file ' + sassErr.file + ' at line ' + sassErr.line);
+            }
+        });
+    });
+}
+
+module.exports = watchSass;
