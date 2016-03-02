1cfcc9d56eacdd68d6e6201ab9ea1a85849d1038
Michael <michaelgira23@gmail.com>
2016-03-02 12:22:52 -0600

    addClass definition

--- src/libs/schedule.js
+++ src/libs/schedule.js
@@ -2,3 +2,22 @@
  * @file Functions for inserting class and schedule data
  * @module schedule
  */
+
+/**
+ * Add a class to the database
+ * @function addClass
+ * 
+ * @param {string} user - Username to insert the class under
+ * 
+ * @param {Object} scheduleClass - JSON of class to add
+ * @param {string} scheduleClass.name - Name of class
+ * @param {string} scheduleClass.teacher - Name of teacher
+ * @param {string} scheduleClass.block - Which block the class takes place
+ * @param {string} scheduleClass.color - Hex value of class color
+ * @param {string} scheduleClass.type - Type of class ('science', 'art', 'math', 'wl', 'english', 'history', or 'other')
+ * @param {Boolean} scheduleClass.displayPlanner - Whether to display the class on the planner
+ */
+
+function addClass(user, scheduleClass) {
+	
+}
