2c9a9e4e1c99a2e50875eb1aa98486b7421bcc4a
MiningPotatoes <catcheese12345@gmail.com>
2016-05-11 12:36:04 -0500

    switching over to pm2, fixing Sass error logs, limit compilation attempts to .scss files

--- src/libs/sass.js
+++ src/libs/sass.js
@@ -16,39 +16,40 @@ var sass = require('node-sass');
 
 function watchSass(sassDir, cssDir) {
     fs.watch(sassDir, function(event, filename) {
-        var sassFile = sassDir + '/' + filename;
-        sass.render({
-            file: sassFile,
-        }, function(sassErr, result) {
-            if(!sassErr) {
-                // Gets raw filename sans .scss extension
-                var cssFilename = filename.replace('.scss', '');
-                // Appends .css file extension
-                cssFilename += '.css';
-                
-                var cssFile = cssDir + '/' + cssFilename;
-                fs.writeFile(cssFile, result.css, function(writeErr) {
-                    if(!writeErr) {
-                        console.log('File ' + sassFile + ' was compiled to ' + cssFile);
-                    } else {
-                        console.log('There was a problem writing the file! Check to make sure node has proper write permissions.');
+        if (filename.substr(filename.lastIndexOf('.') + 1) === "scss") {
+            var sassFile = sassDir + '/' + filename;
+            sass.render({
+                file: sassFile,
+            }, function(sassErr, result) {
+                if(!sassErr) {
+                    // Gets raw filename sans .scss extension
+                    var cssFilename = filename.replace('.scss', '');
+                    // Appends .css file extension
+                    cssFilename += '.css';
+                    
+                    var cssFile = cssDir + '/' + cssFilename;
+                    fs.writeFile(cssFile, result.css, function(writeErr) {
+                        if(!writeErr) {
+                            console.log('File ' + sassFile + ' was compiled to ' + cssFile);
+                        } else {
+                            console.log('There was a problem writing the file! Check to make sure node has proper write permissions.');
+                        }
+                    });
+                } else {
+                    if (sassErr.status != 4) {
+                        completeErr = 'Sass error ' + sassErr.status + ': ' + sassErr.message + ' in file ' + sassErr.file + ' at line ' + sassErr.line + '\n';
+                        logFilename = sassFile + '_err' + new Date().toISOString().substring(0,10) + '.log'; // creates a yyyy-mm-dd timestamp
+                        fs.appendFile(logFilename, completeErr, function(errorErr) {
+                            if(!errorErr) {
+                                console.log("Sass compilation error. Logged to " + logFilename);
+                            } else {
+                                console.log("There was a Sass compilation error, but the log could not be written. Make sure " + logFilename + " can be written to.");
+                            }
+                        });
                     }
-                });
-            } else {
-                completeErr = 'Sass error: ' + sassErr.message + ' in file ' + sassErr.file + ' at line ' + sassErr.line + '\n';
-                /*
-                logFilename = sassFile + '_err' + new Date().toISOString().substring(0,10) + '.log'; // creates a yyyy-mm-dd timestamp
-                fs.appendFile(logFilename, completeErr, function(errorErr) {
-                    if(!errorErr) {
-                        console.log("Sass compilation error. Logged to " + logFilename);
-                    } else {
-                        console.log("There was a Sass compilation error, but the log could not be written. Make sure " + logFilename + " can be written to.");
-                    }
-                });
-                it doesn't really work */
-                console.log(completeErr);
-            }
-        });
+                }
+            });
+        }
     });
 }
 
