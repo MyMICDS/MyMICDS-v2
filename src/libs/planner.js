f2f500cd88352b417e96652e6c50f848cc536425
MiningPotatoes <catcheese12345@gmail.com>
2016-05-12 13:05:06 -0500

    rewording + deleteClass + db.close();

--- src/libs/planner.js
+++ src/libs/planner.js
@@ -3,10 +3,10 @@
  * @module planner
  */
 
-var users = require(__dirname + "/users.js");
-var utils = require(__dirname + "/utils.js");
+var users 		= require(__dirname + "/users.js");
+var utils 		= require(__dirname + "/utils.js");
 var MongoClient = require('mongodb').MongoClient;
-var config      = require(__dirname + '/requireConfig.js');
+var config     	= require(__dirname + '/requireConfig.js');
 
 /**
  * Add/edit event to planner
@@ -21,11 +21,11 @@ var config      = require(__dirname + '/requireConfig.js');
  * @param {string} [event.link] - Link to canvas assignment (optional)
  * @param {string} [id] - Event ID to update (optional)
  * 
- * @param {upsertEventCallback} callback - Callback after planner data is inserted
+ * @param {upsertEventCallback} callback - Callback after planner data is upserted
  */
 
 /**
- * Callback after planner data is inserted
+ * Callback after planner data is upserted
  * @callback upsertEventCallback
  * @param {Boolean} success - True if successful, false if not
  * @param {string} message - Success/error message
@@ -41,8 +41,8 @@ function upsertEvent(user, event, id, callback) {
 	];
 	if(utils.dataIsSet(required)) {
 		MongoClient.connect(config.mongodbURI, function(dbErr, db) {
-			var plannerColl = db.collection("planner");
 			if(!dbErr) {
+				var plannerColl = db.collection("planner");
 				var userId = users.getUserId(user, function(userId) {
 					if(typeof userId !== 'boolean') {
 						if(typeof id === 'undefined') {
@@ -61,11 +61,12 @@ function upsertEvent(user, event, id, callback) {
 
 							plannerColl.insert(insertedEvent, function(insertErr) {
 								if(!insertErr) {
-									callback(true, "Successfully inserted class!");
+									callback(true, "Successfully inserted event!");
 								} else {
-									callback(false, "Error inserting class!");
+									callback(false, "Error inserting event!");
 								}
 							});
+							db.close();
 						} else {
 							// oh hey, there is an id! let's update it.
 							var updatedEvent = {
@@ -81,11 +82,12 @@ function upsertEvent(user, event, id, callback) {
 
 							plannerColl.update({user: userId, _id: id}, {$set: updatedEvent}, function(updateErr) {
 								if(!updateErr) {
-									callback(true, "Successfully updated class!")
+									callback(true, "Successfully updated event!")
 								} else {
-									callback(false, "Error updating class!");
+									callback(false, "Error updating event!");
 								}
 							});
+							db.close();
 						}
 					} else {
 						callback(false, "Error retrieving user ID!");
@@ -102,9 +104,40 @@ function upsertEvent(user, event, id, callback) {
 	}
 }
 
+/**
+ * Deletes events from planner.
+ * @function deleteEvent
+ * 
+ * @param {string} eventId - ID of event to delete.
+ * @param {deleteEventCallback} callback - Callback after planner data is deleted
+ */
 
-function deleteEvent() {
-	// yet more things
+/**
+ * Callback after planner data is deleted
+ * @callback deleteEventCallback
+ * @param {Boolean} success - True if successful, false if not
+ * @param {string} message - Error/success message
+ */
+function deleteEvent(eventId, callback) {
+	if(typeof eventId !== "undefined") {
+		MongoClient.connect(config.mongodbURI, function(dbErr, db) {
+			if(!dbErr) {
+				var plannerColl = db.collection("planner");
+				plannerColl.remove({_id: eventId}, function(removeErr) {
+					if(!removeErr) {
+						callback(true, "Successfully removed event!");
+					} else {
+						callback(false, "Error removing event!");
+					}
+				});
+				db.close();
+			} else {
+				callback(false, "Error connecting to database!");
+			}
+		});
+	} else {
+		callback(false, "Not all required parameters exist!");
+	}
 }
 
 module.exports.upsertEvent = upsertEvent;
