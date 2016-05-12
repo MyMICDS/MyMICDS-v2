8411ee94cec928a4ebe2c4c77762ade3b9e39a49
MiningPotatoes <catcheese12345@gmail.com>
2016-05-12 09:31:45 -0500

    getting somewhere with Mongo?

--- src/libs/planner.js
+++ src/libs/planner.js
@@ -5,14 +5,14 @@
 
 var users = require(__dirname + "/users.js");
 var MongoClient = require('mongodb').MongoClient;
-
+var config      = require(__dirname + '/requireConfig.js');
 
 /**
  * Add event to planner
  * @param {String} user - Username to insert event under
  * @param {Object} event - Event object
  * @param {String} event.desc - Event description
- * @param {String} event.class - Class associated with event
+ * @param {String} event.class - Class associated with event (obj id)
  * @param {String} event.start - Start date of event
  * @param {String} event.end - End date of event
  * @param {addEventCallback} callback - Callback after planner data is inserted
@@ -26,6 +26,21 @@ var MongoClient = require('mongodb').MongoClient;
 
 function addEvent(user, event, callback) {
 	// stuff goes here at some point
+	MongoClient.connect(config.mongodbURI, function(dbErr, db) {
+		var usersColl = db.collection("users");
+		var planner = db.collection("planner");
+		if(!dbErr) {
+			var userId = users.getUserId(user, function(id) {
+				if(typeof id !== 'boolean') {
+					// this means there was no error!
+				} else {
+					// oops, there was an error.
+				}
+			});
+		} else {
+			// whoops, error!
+		}
+	});
 }
 
 function editEvent() {
