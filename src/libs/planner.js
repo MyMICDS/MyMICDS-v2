b33c73a75b8fcfb72024baf782581354e5ba8aa4
MiningPotatoes <catcheese12345@gmail.com>
2016-05-12 10:29:24 -0500

    finished(?) inserting planner event

--- src/libs/planner.js
+++ src/libs/planner.js
@@ -4,17 +4,19 @@
  */
 
 var users = require(__dirname + "/users.js");
+var utils = require(__dirname + "/utils.js");
 var MongoClient = require('mongodb').MongoClient;
 var config      = require(__dirname + '/requireConfig.js');
 
 /**
  * Add event to planner
- * @param {String} user - Username to insert event under
+ * @param {string} user - Username to insert event under
  * @param {Object} event - Event object
- * @param {String} event.desc - Event description
- * @param {String} event.class - Class associated with event (obj id)
- * @param {String} event.start - Start date of event
- * @param {String} event.end - End date of event
+ * @param {string} event.desc - Event description
+ * @param {string} event.class - Class associated with event (obj id)
+ * @param {Object} event.start - Start date of event, Date() object
+ * @param {Object} event.end - End date of event, Date() object
+ * @param {string} [event.link] - Link to canvas assignment (optional)
  * @param {addEventCallback} callback - Callback after planner data is inserted
  */
 
@@ -22,25 +24,56 @@ var config      = require(__dirname + '/requireConfig.js');
  * Callback after planner data is inserted
  * @callback addEventCallback
  * @param {Boolean} success - True if successful, false if not
+ * @param {string} message - Success/error message
  */
 
 function addEvent(user, event, callback) {
-	// stuff goes here at some point
-	MongoClient.connect(config.mongodbURI, function(dbErr, db) {
-		var usersColl = db.collection("users");
-		var planner = db.collection("planner");
-		if(!dbErr) {
-			var userId = users.getUserId(user, function(id) {
-				if(typeof id !== 'boolean') {
-					// this means there was no error!
-				} else {
-					// oops, there was an error.
-				}
-			});
-		} else {
-			// whoops, error!
-		}
-	});
+	var required = [
+		user,
+		event.desc,
+		event.class,
+		event.start,
+		event.end
+	];
+	if(utils.dataIsSet(required)) {
+		MongoClient.connect(config.mongodbURI, function(dbErr, db) {
+			var plannerColl = db.collection("planner");
+			if(!dbErr) {
+				var userId = users.getUserId(user, function(id) {
+					if(typeof id !== 'boolean') {
+						if(dataIsSet)
+						var insertedEvent = {
+							user: id,
+							description: event.desc,
+							class: event.class,
+							start: event.start,
+							end: event.end
+						};
+
+						if(typeof event.link !== "undefined") {
+							insertedEvent["link"] = event.link;
+						}
+
+						plannerColl.insert(insertedEvent, function(insertErr, result) {
+							if(!insertErr) {
+								callback(true, "Successfully inserted class!");
+							} else {
+								callback(false, "Error inserting class!");
+							}
+						});
+					} else {
+						callback(false, "Error retrieving user ID!");
+					}
+				});
+			} else {
+				// whoops, error!
+				callback(false, "Error connecting to database!");
+			}
+		});
+	} else {
+		// look at that, another error.
+		callback(false, "Not all required parameters exist!");
+	}
 }
 
 function editEvent() {
