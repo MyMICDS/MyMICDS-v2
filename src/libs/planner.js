2d065eb8c0d523c0afa593618386a72144fdca01
MiningPotatoes <catcheese12345@gmail.com>
2016-05-12 11:07:37 -0500

    changing it to upsert planner events because Michael said so

--- src/libs/planner.js
+++ src/libs/planner.js
@@ -9,8 +9,8 @@ var MongoClient = require('mongodb').MongoClient;
 var config      = require(__dirname + '/requireConfig.js');
 
 /**
- * Add event to planner
- * @function addEvent
+ * Add/edit event to planner
+ * @function upsertEvent
  * 
  * @param {string} user - Username to insert event under
  * @param {Object} event - Event object
@@ -19,17 +19,19 @@ var config      = require(__dirname + '/requireConfig.js');
  * @param {Object} event.start - Start date of event, Date() object
  * @param {Object} event.end - End date of event, Date() object
  * @param {string} [event.link] - Link to canvas assignment (optional)
- * @param {addEventCallback} callback - Callback after planner data is inserted
+ * @param {string} [id] - Event ID to update (optional)
+ * 
+ * @param {upsertEventCallback} callback - Callback after planner data is inserted
  */
 
 /**
  * Callback after planner data is inserted
- * @callback addEventCallback
+ * @callback upsertEventCallback
  * @param {Boolean} success - True if successful, false if not
  * @param {string} message - Success/error message
  */
 
-function addEvent(user, event, callback) {
+function upsertEvent(user, event, id, callback) {
 	var required = [
 		user,
 		event.desc,
@@ -41,28 +43,50 @@ function addEvent(user, event, callback) {
 		MongoClient.connect(config.mongodbURI, function(dbErr, db) {
 			var plannerColl = db.collection("planner");
 			if(!dbErr) {
-				var userId = users.getUserId(user, function(id) {
-					if(typeof id !== 'boolean') {
-						if(dataIsSet)
-						var insertedEvent = {
-							user: id,
-							description: event.desc,
-							class: event.class,
-							start: event.start,
-							end: event.end
-						};
+				var userId = users.getUserId(user, function(userId) {
+					if(typeof userId !== 'boolean') {
+						if(typeof id === 'undefined') {
+							// no id to update from? that's fine, just insert
+	 						var insertedEvent = {
+								user: userId,
+								description: event.desc,
+								class: event.class,
+								start: event.start,
+								end: event.end
+							};
 
-						if(typeof event.link !== "undefined") {
-							insertedEvent["link"] = event.link;
-						}
-
-						plannerColl.insert(insertedEvent, function(insertErr, result) {
-							if(!insertErr) {
-								callback(true, "Successfully inserted class!");
-							} else {
-								callback(false, "Error inserting class!");
+							if(typeof event.link !== "undefined") {
+								insertedEvent["link"] = event.link;
 							}
-						});
+
+							plannerColl.insert(insertedEvent, function(insertErr) {
+								if(!insertErr) {
+									callback(true, "Successfully inserted class!");
+								} else {
+									callback(false, "Error inserting class!");
+								}
+							});
+						} else {
+							// oh hey, there is an id! let's update it.
+							var updatedEvent = {
+								description: event.desc,
+								class: event.class,
+								start: event.start,
+								end: event.end
+							};
+
+							if(typeof event.link !== "undefined") {
+								updatedEvent["link"] = event.link;
+							}
+
+							plannerColl.update({user: userId, _id: id}, {$set: updatedEvent}, function(updateErr) {
+								if(!updateErr) {
+									callback(true, "Successfully updated class!")
+								} else {
+									callback(false, "Error updating class!");
+								}
+							});
+						}
 					} else {
 						callback(false, "Error retrieving user ID!");
 					}
@@ -78,14 +102,10 @@ function addEvent(user, event, callback) {
 	}
 }
 
-function editEvent() {
-	// and more stuff
-}
 
 function deleteEvent() {
 	// yet more things
 }
 
-module.exports.addEvent = addEvent;
-module.exports.editEvent = editEvent;
+module.exports.upsertEvent = upsertEvent;
 module.exports.deleteEvent = deleteEvent;
