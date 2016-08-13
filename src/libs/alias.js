12ed83c652f12b59518a45df8acac21757ee060a
nickle <catcheese12345@gmail.com>
2016-08-13 18:19:30 -0500

    now, time to retrieve aliases

--- src/libs/alias.js
+++ src/libs/alias.js
@@ -5,7 +5,8 @@
  * @module alias
  */
 
-var ObjectID   = require('mongodb').ObjectID;
+var _ 		 = require('underscore');
+var ObjectID = require('mongodb').ObjectID;
 
 /**
  * Aliases a portal class with a native class
@@ -74,5 +75,65 @@ function addCanvasAlias(db, classString, classID, callback) {
 		}
 
 		callback(null);
+	});
+}
+
+/**
+ * Check if given class has a portal alias
+ * @function getPortalAlias
+ * 
+ * @param {Object} db - Database object
+ * @param {string} classInput - Class to check for an alias
+ * @param {getPortalAliasCallback} callback - Callback
+ */
+
+/**
+ * Callback after alias is checked
+ * @callback getPortalAliasCallback
+ *
+ * @param {Object} err - Null if success, error object if failure
+ * @param {string} classOutput - Class ID if alias found, inputted class if not found, null if failure
+ */
+function getPortalAlias(db, classInput, callback) {
+	var aliases = db.collection("aliases");
+
+	aliases.find({type: "portal", classRemote: classInput}).toArray(function(err, aliases) {
+		if(err) {
+			callback(new Error("There was an error searching for aliases!"), null);
+		} else if(_.isEmpty(aliases)) {
+			callback(null, classInput);
+		} else {
+			callback(null, aliases[0].classNative);
+		}
+	});
+}
+
+/**
+ * Check if given class has a canvas alias
+ * @function getCanvasAlias
+ * 
+ * @param {Object} db - Database object
+ * @param {string} classInput - Class to check for an alias
+ * @param {getCanvasAliasCallback} callback - Callback
+ */
+
+/**
+ * Callback after alias is checked
+ * @callback getCanvasAliasCallback
+ *
+ * @param {Object} err - Null if success, error object if failure
+ * @param {string} classOutput - Class ID if alias found, inputted class if not found, null if failure
+ */
+function getCanvasAlias(db, classInput, callback) {
+	var aliases = db.collection("aliases");
+
+	aliases.find({type: "canvas", classRemote: classInput}).toArray(function(err, aliases) {
+		if(err) {
+			callback(new Error("There was an error searching for aliases!"), null);
+		} else if(_.isEmpty(aliases)) {
+			callback(null, classInput);
+		} else {
+			callback(null, aliases[0].classNative);
+		}
 	});
 }
