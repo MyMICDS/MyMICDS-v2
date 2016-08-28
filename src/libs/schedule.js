9f07f9a2babc5c10dcfb3bd00392189086230c18
Michael Gira <michaelgira23@gmail.com>
2016-08-27 21:51:31 -0500

    Return middle school schedule

--- src/libs/schedule.js
+++ src/libs/schedule.js
@@ -9,7 +9,11 @@ var _ = require('underscore');
 var users = require(__dirname + '/users.js');
 
 // Schedules
-var highschoolSchedule = require(__dirname + '/../schedules/highschool.json');
+var highschoolSchedule   = require(__dirname + '/../schedules/highschool.json');
+var eighthGradeSchedule  = require(__dirname + '/../schedules/grade8.json');
+var seventhGradeSchedule = require(__dirname + '/../schedules/grade7.json');
+var sixthGradeSchedule   = require(__dirname + '/../schedules/grade6.json');
+var fifthGradeSchedule   = require(__dirname + '/../schedules/grade5.json');
 
 var validBlocks = [
 	'a',
@@ -18,7 +22,13 @@ var validBlocks = [
 	'd',
 	'e',
 	'f',
-	'g'
+	'g',
+	'activities'
+	'advisory',
+	'collaborative',
+	'community',
+	'enrichment',
+	'lunch'
 ];
 
 var validTypes = [
@@ -40,6 +50,115 @@ var validTypes = [
  */
 
 function getSchedule(day, lateStart, grade, blocks) {
+
+	if(typeof blocks !== 'object') return null;
+
+	// Add default blocks
+	if(typeof blocks.activities === 'undefined') {
+		blocks.activities = {
+			name: 'Activities',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#FF6347'
+		};
+	}
+	if(typeof blocks.advisory === 'undefined') {
+		blocks.activities = {
+			name: 'Advisory',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#EEE' // Sophisticated white
+		};
+	}
+	if(typeof blocks.collaborative === 'undefined') {
+		blocks.collaborative = {
+			name: 'Collaborative Work',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#29ABE2'
+		};
+	}
+	if(typeof blocks.community === 'undefined') {
+		blocks.community = {
+			name: 'Community',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#AA0031'
+		};
+	}
+	if(typeof blocks.enrichment === 'undefined') {
+		blocks.activities = {
+			name: 'Enrichment',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#FF4500'
+		};
+	}
+	if(typeof blocks.flex === 'undefined') {
+		blocks.flex = {
+			name: 'Flex',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#CC33FF'
+		};
+	}
+	if(typeof blocks.lunch === 'undefined') {
+		blocks.activities = {
+			name: 'Lunch!',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#116C53'
+		};
+	}
+	if(typeof blocks.recess === 'undefined') {
+		blocks.activities = {
+			name: 'Recess',
+			teacher: {
+				prefix: '',
+				firstName: '',
+				lastName: ''
+			},
+			type: 'other',
+			block: 'other',
+			color: '#FFFF00'
+		};
+	}
+
 	if(typeof day !== 'number' || day % 1 !== 0 || 1 > grade || grade > 6) {
 		return null;
 	}
@@ -51,18 +170,14 @@ function getSchedule(day, lateStart, grade, blocks) {
 	var schoolName = users.gradeToSchool(grade);
 
 	// If lowerschool, return null
-	if(schoolName === 'lowerschool') {
-		return null;
-	}
+	if(schoolName === 'lowerschool') return null;
 
 	// Make sure all blocks are valid
 	for(var i = 0; i < validBlocks.length; i++) {
 		var block = validBlocks[i];
 
 		// Make sure block is in blocks
-		if(typeof blocks[block] === 'undefined') {
-			return null;
-		}
+		if(typeof blocks[block] === 'undefined') return null;
 	}
 
 	// User's final schedule
@@ -119,18 +234,91 @@ function getSchedule(day, lateStart, grade, blocks) {
 			}
 
 			// Push to user schedule
-			userSchedule.push(blocks[jsonBlock.type]);
+			userSchedule.push({
+				start: jsonBlock.start,
+				end  : jsonBlock.end,
+				block: jsonBlock.type
+				class: blocks[jsonBlock.type]
+			});
 		}
 
 		return userSchedule;
 
 	} else if(grade === 8) {
 
+		// Loop through JSON and append classes to user schedule
+		var jsonSchedule = eighthGradeSchedule['day' + day][lateStart ? 'lateStart' : 'regular'];
+
+		for(var i = 0; i < jsonSchedule.length; i++) {
+			var jsonBlock = jsonSchedule[i];
+
+			// Push to user schedule
+			userSchedule.push({
+				start: jsonBlock.start,
+				end  : jsonBlock.end,
+				block: jsonBlock.type
+				class: blocks[jsonBlock.type]
+			});
+		}
+
+		return userSchedule;
+
 	} else if(grade === 7) {
+
+		// Loop through JSON and append classes to user schedule
+		var jsonSchedule = seventhGradeSchedule['day' + day][lateStart ? 'lateStart' : 'regular'];
+
+		for(var i = 0; i < jsonSchedule.length; i++) {
+			var jsonBlock = jsonSchedule[i];
+
+			// Push to user schedule
+			userSchedule.push({
+				start: jsonBlock.start,
+				end  : jsonBlock.end,
+				block: jsonBlock.type
+				class: blocks[jsonBlock.type]
+			});
+		}
+
+		return userSchedule;
 
 	} else if(grade === 6) {
 
+		// Loop through JSON and append classes to user schedule
+		var jsonSchedule = sixthGradeSchedule['day' + day][lateStart ? 'lateStart' : 'regular'];
+
+		for(var i = 0; i < jsonSchedule.length; i++) {
+			var jsonBlock = jsonSchedule[i];
+
+			// Push to user schedule
+			userSchedule.push({
+				start: jsonBlock.start,
+				end  : jsonBlock.end,
+				block: jsonBlock.type
+				class: blocks[jsonBlock.type]
+			});
+		}
+
+		return userSchedule;
+
 	} else if(grade === 5) {
+
+		// Loop through JSON and append classes to user schedule
+		var jsonSchedule = fifthGradeSchedule['day' + day][lateStart ? 'lateStart' : 'regular'];
+
+		for(var i = 0; i < jsonSchedule.length; i++) {
+			var jsonBlock = jsonSchedule[i];
+
+			// Push to user schedule
+			userSchedule.push({
+				start: jsonBlock.start,
+				end  : jsonBlock.end,
+				block: jsonBlock.type
+				class: blocks[jsonBlock.type]
+			});
+		}
+
+		return userSchedule;
 
 	} else {
 		return null;
