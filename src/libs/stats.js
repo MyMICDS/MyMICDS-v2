e999d7af6edf274af05555d60036740b807c2c9f
nickle <catcheese12345@gmail.com>
2016-10-07 18:06:53 -0500

    change stats to return dates of registration

--- src/libs/stats.js
+++ src/libs/stats.js
@@ -32,25 +32,58 @@ function getStats(db, callback) {
 			return;
 		}
 
-		userdata.find({lastVisited: {$gte: moment().startOf('day').toDate()}}).count(function(err, visitCount) {
+		userdata.distinct('gradYear', function(err, gradYears) {
 			if(err) {
-				callback(new Error('There was an error finding the recently active users!'), null);
+				callback(new Error('There was an error getting the user graduation years!'), null);
 				return;
 			}
 
-			userdata.find({registered: {$gte: moment().startOf('day').toDate()}}).count(function(err, registerCount) {
-				if(err) {
-					callback(new Error('There was an error finding the recently registered users!'), null);
-					return;
-				}
-			
-				var statistics = {
-					totalUsers: userCount,
-					visitedToday: visitCount,
-					registeredToday: registerCount
-				};
+			var yearDates = {};
 
-				callback(null, statistics);
+			gradYears.forEach(function(year) {
+				yearDates[year] = {};
+
+				userdata.find({gradYear: year}).toArray(function(err, userDocs) {
+					if(err) {
+						callback(new Error('There was an error getting the user register dates!'), null);
+						return;
+					}
+
+					userDocs.forEach(function(userDoc) {
+						var registerDay = moment(userDoc['registered']).format('YYYY-MM-DD');
+
+						if(Object.keys(yearDates[year]).includes(registerDay)) {
+							yearDates[year][registerDay]++;
+						} else {
+							yearDates[year][registerDay] = 1;
+						}
+					});
+
+					userdata.find({lastVisited: {$gte: moment().startOf('day').toDate()}}).count(function(err, visitCount) {
+						if(err) {
+							callback(new Error('There was an error finding the recently active users!'), null);
+							return;
+						}
+
+						userdata.find({registered: {$gte: moment().startOf('day').toDate()}}).count(function(err, registerCount) {
+							if(err) {
+								callback(new Error('There was an error finding the recently registered users!'), null);
+								return;
+							}
+						
+							var statistics = {
+								registered: {
+									total: userCount,
+									today: registerCount,
+									gradYears: yearDates
+								},
+								visitedToday: visitCount
+							};
+
+							callback(null, statistics);
+						});
+					});
+				});
 			});
 		});
 	});
