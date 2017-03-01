13841dae658479fe22845e0e5b18b7d96a2b055f
Michael Gira <contact@michaelgira.me>
2017-03-01 12:55:42 -0600

    Add test to see if usernames with hyphens work

---
+++ src/test/backgrounds.js
@@ -0,0 +1,5 @@
+var backgrounds = require(__dirname + '/../libs/backgrounds.js');
+
+backgrounds.get('bhollander-bodie', function(err, variants, hasDefault) {
+	console.log(err, variants, hasDefault);
+});
