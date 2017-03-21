45a8433cddb268a13088da177b7adb473790685f
Michael Gira <michaelgira23@gmail.com>
2017-03-20 21:09:33 -0400

    Alphabeticalize the router imports

--- src/index.js
+++ src/index.js
@@ -90,10 +90,10 @@ MongoClient.connect(config.mongodb.uri, function(err, db) {
 	require(__dirname + '/routes/snowdayAPI.js')(app, db);
 	require(__dirname + '/routes/sportsAPI.js')(app);
 	require(__dirname + '/routes/statsAPI.js')(app, db);
+	require(__dirname + '/routes/suggestionAPI.js')(app, db);
 	require(__dirname + '/routes/teacherAPI.js')(app, db);
 	require(__dirname + '/routes/userAPI.js')(app, db, socketIO);
 	require(__dirname + '/routes/weatherAPI.js')(app, db, socketIO);
-	require(__dirname + '/routes/suggestionAPI.js')(app, db);
 });
 
 app.get('/start', function(req, res) {
