71e2ac87c859d562248242822b42d7867d3d7db0
Nick Clifford <catcheese12345@gmail.com>
2017-07-28 14:30:24 -0400

    copypasterino Portal function (also untested)

--- src/libs/feeds.js
+++ src/libs/feeds.js
@@ -65,4 +65,64 @@ function processCanvasQueue(db, callback) {
 	});
 }
 
+/**
+ * Process the queue for updating cached Portal feeds
+ * @param {Object} db - Database object
+ * @param {processPortalQueueCallback} callback - Callback
+ */
+
+/**
+ * Returns an error if any
+ * @callback processPortalQueueCallback
+ *
+ * @param {Object} err - Null if success, error object if failure
+ */
+
+function processPortalQueue(db, callback) {
+	if(typeof callback !== 'function') return;
+
+	if(typeof db !== 'object') {
+		callback(new Error('Invalid database connection!'));
+		return;
+	}
+
+	const portaldata = db.collection('portalFeeds');
+	const userdata = db.collection('users');
+
+	userdata.find({ inPortalQueue: true }).toArray((err, queue) => {
+		if(err) {
+			callback(new Error('There was a problem querying the database!'));
+			return;
+		}
+
+		function handleQueue(i) {
+			if(i >= queue.length) return;
+
+			const userDoc = queue[i];
+
+			portal.getCal(db, userDoc.user, (err, hasURL, events) => {
+				if(err) {
+					callback(err);
+					return;
+				}
+
+				events.forEach(e => e.user = userDoc._id);
+				console.log(events);
+
+				portaldata.insertMany(events, err => {
+					if(err) {
+						callback('There was an error inserting events into the database!');
+						return;
+					}
+				});
+			});
+
+			handleQueue(++i);
+		}
+
+		handleQueue(0);
+	});
+}
+
 module.exports.processCanvasQueue = processCanvasQueue;
+module.exports.processPortalQueue = processPortalQueue;
