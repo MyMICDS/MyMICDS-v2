fc3f926179571a05aeb6649628015181eecc9e6e
AsianPsychoBoy <ziyang_99@outlook.com>
2018-06-26 00:06:36 -0500

    work on advisory api and assigning snack duty

--- src/libs/snacklist.js
+++ src/libs/snacklist.js
@@ -36,10 +36,24 @@ function getAdvisory(db, user, callback) {
 			return;
 		}
 		studentAdvisor.aggregate([
-			{ $match: { advisor: doc.advisor } },
-			{ $lookup: { from: 'users', localField: 'user', foreignField: 'user', as: 'user' } },
-			{ $replaceRoot: { newRoot: { $arrayElemAt: [ '$user', 0 ] } } },
-			{ $project: { _id: 0, user: 1, firstName: 1, lastName: 1 } }
+			{
+				$match: { advisor: doc.advisor }
+			},
+			{
+				$lookup: { from: 'users', localField: 'user', foreignField: 'user', as: 'user' }
+			},
+			{
+				$replaceRoot: { newRoot: { $arrayElemAt: [ '$user', 0 ] } }
+			},
+			{
+				$lookup: { from: 'snackDuty', localField: 'user', foreignField: 'user', as: 'snackDutyTemp' }
+			},
+			{
+				$replaceRoot: { newRoot: { $mergeObjects: [ { $arrayElemAt: [ '$snackDutyTemp', 0 ] }, '$$ROOT' ] } }
+			},
+			{
+				$project: { _id: 0, user: 1, firstName: 1, lastName: 1, snackDuty: 1 }
+			}
 		]).toArray((err, docs) => {
 			if (err) {
 				callback(new Error(err), null);
@@ -55,10 +69,14 @@ function getAdvisory(db, user, callback) {
 
 function getAdvisoryDays(callback) {
 	portal.getDayRotations((err, days) => {
-		const secondYear = new Date().getMonth < 6;
-		const currYear = new Date().getFullYear();
+		let numberOfDays = 0;
+
+		const currDate = new Date(2019, 3, 1);
+		const secondYear = currDate.getMonth() + 1 < 6;
+		const currYear = currDate.getFullYear();
 		const startYear = secondYear ? currYear - 1 : currYear;
-		const currMonth = new Date().getMonth();
+		const currMonth = currDate.getMonth() + 1;
+		// console.log(secondYear, currYear, startYear)
 		for (let year = startYear; year <= currYear; year++) {
 			let startMonth = 0;
 			let endMonth = 0;
@@ -74,18 +92,42 @@ function getAdvisoryDays(callback) {
 				startMonth = 8;
 				endMonth = currMonth;
 			}
-			for (let month = startMonth; month < endMonth; month++) {
-				days[year][month].keys.forEach(day => {
-					if (days[year][month][day] === ) {}
+			// console.log(startMonth, endMonth)
+			for (let month = startMonth; month <= endMonth; month++) {
+				let daysKeys = [];
+				try {
+					daysKeys = Object.keys(days[year][month]);
+				} catch (e) {
+					callback(new Error('School is not in session.'), null);
+					return;
+				}
+				daysKeys.forEach(day => {
+					let date = new Date(year, month - 1, day);
+					if (date.valueOf() > currDate.valueOf()) {
+						return;
+					}
+
+					let dayRotation = days[year][month][day];
+					// console.log(year, month, day, dayRotation, !(
+					// 	date.getDay() === 3 && // Not Wednesday late starts but...
+					// 	(dayRotation !== 1 && dayRotation !== 4) // Day 1 and Day 4 late starts have advisory
+					// ))
+					if (!(
+						date.getDay() === 3 && // Not Wednesday late starts but...
+						(dayRotation !== 1 && dayRotation !== 4) // Day 1 and Day 4 late starts have advisory
+					)) {
+						numberOfDays++;
+					}
 				});
 			}
 		}
-		callback(err, days);
+		callback(err, numberOfDays);
 	});
 }
 
 function giveSnackDuty(db) {
 	const studentAdvisor = db.collection('studentAdvisor');
+	const snackDuty = db.collection('snackDuty');
 
 	studentAdvisor.aggregate([
 		{ $group: { _id: '$advisor', students: { $push: '$user' } } }
@@ -94,12 +136,28 @@ function giveSnackDuty(db) {
 			console.log(err);
 			return;
 		}
-		getAdvisoryDays((err, days) => {
+
+		getAdvisoryDays((err, numberOfDays) => {
 			if (err) {
 				console.log(err);
 				return;
 			}
-			console.log(err, docs);
+			console.log(err, docs, numberOfDays);
+			docs.forEach((doc) => {
+				const nextStudent = doc.students[numberOfDays % doc.students.length];
+				console.log({ user: nextStudent });
+				snackDuty.update({ advisor: doc._id }, {
+					advisor: doc._id,
+					user: nextStudent,
+					snackDuty: true
+				}, { upsert: true }, (err) => {
+					if (err) {
+						console.log(new Error(err));
+						return;
+					}
+					console.log('Update snack duty success.');
+				});
+			});
 		});
 	});
 }
