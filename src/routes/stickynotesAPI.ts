b989a2827233aea0e8c767932c3b713cddb15e77
Michael Gira <contact@michaelgira.me>
2019-01-03 15:49:14 -0600

    Fix get stickynotes from url param

--- src/routes/stickynotesAPI.ts
+++ src/routes/stickynotesAPI.ts
@@ -6,7 +6,7 @@ export default ((app, db) => {
 
 	app.get('/stickynotes', async (req, res) => {
 		try {
-			const note = await stickynotes.get(db, req.user.user, req.body.moduleId);
+			const note = await stickynotes.get(db, req.user.user, req.query.moduleId);
 			api.success(res, { stickynote: note });
 		} catch (err) {
 			api.error(res, err);
