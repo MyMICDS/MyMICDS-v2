98ddb01f77f3baedd80c028427516fe2c2b4e1fd
Michael Gira <michael@gira.dev>
2019-08-19 23:42:20 -0500

    Update lunch module to reflect Flik's new API

--- src/external.d.ts
+++ src/external.d.ts
@@ -2,6 +2,7 @@ declare module 'forecast.io';
 declare module 'googleapis';
 declare module 'google-batch';
 declare module 'ical';
+declare module 'object-assign-deep';
 declare module 'prisma';
 
 // Needed for @mymicds/sdk
--- src/libs/lunch.ts
+++ src/libs/lunch.ts
@@ -1,13 +1,16 @@
-import { GetLunchResponse, School } from '@mymicds/sdk';
-import * as cheerio from 'cheerio';
+import { GetLunchResponse, School, SchoolLunch } from '@mymicds/sdk';
 import moment from 'moment';
 import { Db } from 'mongodb';
+import objectAssignDeep from 'object-assign-deep';
 import { Response } from 'request';
 import request from 'request-promise-native';
-import * as utils from './utils';
 
-const lunchURL = 'https://myschooldining.com/MICDS/calendarWeek';
-const schools = ['Lower School', 'Middle School', 'Upper School'];
+const lunchBaseURL = 'https://micds.flikisdining.com/menu/api/weeks/school/mary-institute-country-day-school-micds/menu-type';
+const schools: Record<School, string> = {
+	lowerschool: 'lunch',
+	middleschool: 'middle-school-menu',
+	upperschool: 'upper-school-menu'
+};
 
 /**
  * Gets the lunch from Flik's website.
@@ -18,20 +21,28 @@ async function getLunch(db: Db, date: Date) {
 	if (typeof db !== 'object') { throw new Error('Invalid database connection!'); }
 
 	const currentDay = moment(date).day('Wednesday');
+	const fullLunchResponse: GetLunchResponse['lunch'] = {};
 
 	let res: Response;
 	try {
-		// Send POST request to lunch website
-		res = await request.post(lunchURL, {
-			form: { current_day: currentDay.format() },
-			resolveWithFullResponse: true,
-			simple: false
-		});
-	} catch (e) {
-		throw new Error('There was a problem fetching the lunch data!');
-	}
+		for (const school of Object.keys(schools) as School[]) {
+			const schoolUrl = schools[school];
+			const lunchUrl =
+				`${lunchBaseURL}/${schoolUrl}/${currentDay.year()}/${currentDay.month() + 1}/${currentDay.date()}`;
+			// Send POST request to lunch website
+			res = await request.get(lunchUrl, {
+				resolveWithFullResponse: true,
+				simple: false,
+				json: true
+			});
 
-	if (res.statusCode !== 200) { throw new Error('It appears the lunch site is down. Check again later!'); }
+			// if (res.statusCode !== 200) { throw new Error('It appears the lunch site is down. Check again later!'); }
+
+			objectAssignDeep(fullLunchResponse, parseLunch(schoolFilter(school), res.body));
+		}
+	} catch (e) {
+		throw new Error('There was a problem fetching the lunch data!' + e);
+	}
 
 	// Alert admins if lunch page has moved
 	// admins.sendEmail(db, {
@@ -45,72 +56,39 @@ async function getLunch(db: Db, date: Date) {
 	// 	console.log('[' + new Date() + '] Alerted admins of error! (' + err + ')');
 	// });
 
-	// callback(new Error('There was a problem with the lunch URL!'), null);
-
-	return parseLunch(res.body as string);
+	return fullLunchResponse;
 }
 
 /**
- * Scrapes Flik's lunch page into JSON data.
+ * Parses Flik's JSON data into our API
  * @param body HTML body of the page.
  * @returns An object with the lunch data.
  */
-function parseLunch(body: string) {
-	// Clean up HTML to prevent cheerio from becoming confused
-	body.replace('<<', '&lt;&lt;');
-	body.replace('>>', '&gt;&gt;');
+function parseLunch(school: School, body: any) {
+	const json: any = {};
 
-	const $ = cheerio.load(body);
-	const json: GetLunchResponse['lunch'] = {};
+	for (const day of body.days) {
+		const date = day.date;
 
-	const table = $('table#table_calendar_week');
-	const weekColumns = table.find('td');
+		json[date] = {
+			[school]: {
+				title: 'Yummy Lunch',
+				categories: {}
+			}
+		};
 
-	weekColumns.each(function(this: any) {
-		// I'm having trouble importing some of the Cheerio types, but we're only using the `this` value here
-		// so it doesn't really matter exactly what type it is
-		const day = $(this);
-		const date = day.attr('this_date');
-		const dateObject = new Date(date);
-		const dateString = dateObject.getFullYear()
-			+ '-' + utils.leadingZeros(dateObject.getMonth() + 1)
-			+ '-' + utils.leadingZeros(dateObject.getDate());
-
-		for (const school of schools) {
-			const schoolLunch = day.find('div[location="' + school + '"]');
-
-			// Make sure it's not the weekend
-			if (schoolLunch.length > 0) {
-				const lunchTitle = schoolLunch.find('span.period-value').text().trim();
-				const categories = schoolLunch.find('div.category-week');
-
-				categories.each(function(this: any) {
-					const category = $(this);
-					const food: string[] = [];
-					const categoryTitle = category.find('span.category-value').text().trim();
-					const items = category.find('div.item-week');
-
-					items.each(function(this: any) {
-						food.push($(this).text().trim());
-					});
-
-					// Add to JSON
-					json[dateString] = json[dateString] || {};
-					json[dateString][schoolFilter(school)] = json[dateString][schoolFilter(school)] || {};
-
-					json[dateString][schoolFilter(school)].title = lunchTitle;
-					json[dateString][schoolFilter(school)].categories =
-						json[dateString][schoolFilter(school)].categories || {};
-					json[dateString][schoolFilter(school)].categories[categoryTitle] =
-						json[dateString][schoolFilter(school)].categories[categoryTitle] || [];
-
-					for (const f of food) {
-						json[dateString][schoolFilter(school)].categories[categoryTitle].push(f);
-					}
-				});
+		let latestCategory = null;
+		for (const item of day.menu_items) {
+			if (item.text) {
+				latestCategory = item.text;
+			} else if (latestCategory && item.food) {
+				if (!json[date][school].categories[latestCategory]) {
+					json[date][school].categories[latestCategory] = [];
+				}
+				json[date][school].categories[latestCategory].push(item.food.name);
 			}
 		}
-	});
+	}
 
 	return json;
 }
@@ -121,7 +99,7 @@ function parseLunch(body: string) {
  * @returns A school name formatted for JSON use.
  */
 function schoolFilter(school: string) {
-	return school.replace(/\s+/g, '').toLowerCase() as School;
+	return school.replace('-', '') as School;
 }
 
 export {
