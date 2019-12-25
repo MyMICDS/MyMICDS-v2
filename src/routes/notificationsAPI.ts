d610f0c86b7e6e388c8843986d5337193377dffc
Nick Clifford <nick@nickclifford.me>
2019-12-25 15:38:39 -0600

    finish testing, fix notifications validation

--- src/routes/notificationsAPI.ts
+++ src/routes/notificationsAPI.ts
@@ -1,4 +1,4 @@
-import { UnsubscribeParameters } from '@mymicds/sdk';
+import { Scope } from '@mymicds/sdk';
 import { assertType } from 'typescript-is';
 import * as api from '../libs/api';
 import * as notifications from '../libs/notifications';
@@ -7,17 +7,24 @@ import RoutesFunction from './routesFunction';
 export default ((app, db) => {
 
 	app.post('/notifications/unsubscribe', async (req, res) => {
+		// Union type is being difficult so we need to split this up
 		try {
-			assertType<UnsubscribeParameters>(req.body);
+			assertType<{ scopes: Scope | Scope[] }>(req.body);
 		} catch (err) {
 			api.error(res, err);
 			return;
 		}
 
 		let user = req.apiUser;
-		let hash = true;
+		let hash: string | true = true;
 
 		if (!user) {
+			try {
+				assertType<{ user: string, hash: string }>(req.body);
+			} catch (err) {
+				api.error(res, err);
+				return;
+			}
 			user = req.body.user;
 			hash = req.body.hash;
 		}
