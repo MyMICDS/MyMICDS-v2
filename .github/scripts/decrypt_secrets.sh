19f3998e378057831329e18951589d51a91644de
Nick Clifford <nick@nickclifford.me>
2020-07-09 12:21:00 -0500

    delete github folder to troubleshoot

--- .github/scripts/decrypt_secrets.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-openssl aes-256-cbc -K $FILE_DECRYPT_KEY -iv $FILE_DECRYPT_IV -in .github/secrets/id_rsa.enc -out .github/secrets/id_rsa -d
-openssl aes-256-cbc -K $FILE_DECRYPT_KEY -iv $FILE_DECRYPT_IV -in .github/secrets/config.libs.ts.enc -out src/libs/config.ts -d
-openssl aes-256-cbc -K $FILE_DECRYPT_KEY -iv $FILE_DECRYPT_IV -in .github/secrets/config.test.ts.enc -out test/config.ts -d
