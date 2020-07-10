c6c44e591ed0ded48482ebd292ba8b3915d5289b
Nick Clifford <nick@nickclifford.me>
2020-07-10 11:14:35 -0500

    add known hosts file

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -4,6 +4,10 @@ eval "$(ssh-agent -s)"
 chmod 600 .github/secrets/id_rsa
 ssh-add .github/secrets/id_rsa
 
+# set up known hosts
+mkdir -p ~/.ssh
+ssh-keyscan -p $PORT $IP >> ~/.ssh/known_hosts
+
 ssh apps@$IP -p $PORT <<EOF
   cd $DEPLOY_DIR
   git pull
