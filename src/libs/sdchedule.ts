180c6e7010e3bf492dd6744bc112a4f76255f80f
sam <sbbaumo@yahoo.com>
2020-08-17 13:06:31 -0500

    mew stuff

--- src/libs/sdchedule.ts
+++ src/libs/sdchedule.ts
@@ -1,4 +1,4 @@
-import { Block, ClassType, GetScheduleResponse, ScheduleClass } from '@mymicds/sdk';
+import { Block, ClassType, GetScheduleResponse, ScheduleClass, ScheduleBlock } from '@mymicds/sdk';
 import { Db } from 'mongodb';
 import { StringDict } from './utils';
 import * as _ from 'lodash';
@@ -8,7 +8,7 @@ import * as classes from './classes';
 import * as feeds from './feeds';
 import * as portal from './portal';
 import * as users from './users';
-import moment from 'moment';
+import moment, { Moment } from 'moment';
 import prisma from 'prisma';
 
 import lateStarts from '../schedules/2020/late_starts.json';
@@ -227,7 +227,7 @@ async function getSchedule(
 	}
 
 	// combine everything
-	userSchedule.classes = combineClassesSchedule(requestedDate, dayBlockSchedule, blocks);
+	userSchedule.classes = combineClassesSchedule(requestedDate, dayBlockSchedule.blocks, blocks);
 
 	// if there is no portal URL, we can stop here
 	if (isPortalBroken || typeof userDoc!.portalURLClasses !== 'string') {
@@ -269,10 +269,189 @@ async function getSchedule(
 		return getSchedule(db, user, date, true);
 	}
 
-	let portalSchedule: ScheduleClasses = [];
+	const schoolScheduleEvents = [];
 
-	//schedule.classes = ordineSchedule([], schedule.classes);
+	if (portalCalendarResult.hasURL && portalCalendarResult.cal) {
+		// Go through all the events in the Portal calendar
+		for (const calEvent of Object.values(
+			portalCalendarResult.cal as portal.PortalCacheEvent[]
+		)) {
+			const start = moment(calEvent.start);
+			const end = moment(calEvent.end);
+
+			// It doesn't make any sense for end to come before start
+			// But I guess it's theoretically possible so we should check for it
+			if (end.isBefore(start)) {
+				continue;
+			}
+
+			// Check if event occurs on specified day
+			if (requestedDate.isSame(start, 'day')) {
+				// Check if special schedule
+				const lowercaseSummary = calEvent.summary!.toLowerCase();
+				if (
+					lowercaseSummary.includes('special') ||
+					lowercaseSummary.includes('ss') ||
+					lowercaseSummary.includes('modified')
+				) {
+					userSchedule.special = true;
+					continue;
+				}
+
+				// Check if event occurs throughout school day
+				if (start.isSameOrAfter(defaultStart) && end.isSameOrBefore(defaultEnd)) {
+					const color = prisma(calEvent.summary).hex;
+					schoolScheduleEvents.push({
+						start,
+						end,
+						class: {
+							portal: true,
+							name: calEvent.summary,
+							teacher: {
+								prefix: '',
+								firstName: '',
+								lastName: ''
+							},
+							block: 'other',
+							type: 'other',
+							color,
+							textDark: prisma.shouldTextBeDark(color)
+						}
+					});
+				}
+			}
+			// Check if it's an all-day event
+			// @TODO Don't know if this works (if everything we'd consider "all-day" event actually matches this criteria)
+			if (start.isSameOrBefore(requestedDate) && end.isSameOrAfter(requestedDateNextDay)) {
+				userSchedule.allDay.push(portal.cleanUp(calEvent.summary!));
+			}
+		}
+	}
+
+	const portalSchedule: ScheduleClasses = [];
+
+	// Go through ALL the events in the Portal classes
+	for (const calEvent of Object.values(portalClassesResult.cal as portal.PortalCacheEvent[])) {
+		const start = moment(calEvent.start);
+		const end = moment(calEvent.end);
+
+		// make sure event doesn't violate the 4th dimension and end before it starts
+		if (end.isBefore(start)) {
+			continue;
+		}
+
+		// Check if it's an all-day event
+		if (start.isSameOrBefore(requestedDate) && !end.isValid()) {
+			// Push event to all-day events
+			userSchedule.allDay.push(portal.cleanUp(calEvent.summary!));
+		} else if (start.isAfter(requestedDate) && end.isBefore(requestedDateNextDay)) {
+			// We should use the Portal class's alias; otherwise, we should fallback to a default class object. [sp1a]
+			if (!(calEvent.summary! in aliasesResult.portal)) {
+				// Determine block
+				// eslint-disable-next-line @typescript-eslint/prefer-regexp-exec
+				const blockPart = _.last(calEvent.summary!.match(portal.portalSummaryBlock));
+				let block = Block.OTHER;
+
+				if (blockPart) {
+					block = _.last(blockPart.match(/[A-H]/g))!.toLowerCase() as Block;
+				}
+
+				// Generate random color
+				const color = prisma(calEvent.summary).hex;
+
+				// RegEx for determining block and stuff is a bit intense; therefore, we should cache it. [sp1a]
+				aliasesResult.portal[calEvent.summary!] = {
+					portal: true,
+					name: portal.cleanUp(calEvent.summary!),
+					teacher: {
+						prefix: '',
+						firstName: '',
+						lastName: ''
+					},
+					block,
+					type: ClassType.OTHER,
+					color,
+					textDark: prisma.shouldTextBeDark(color)
+				};
+			}
+
+			// Add block into Portal Schedule array
+			portalSchedule.push({
+				class: aliasesResult.portal[calEvent.summary!] as ScheduleClass,
+				start,
+				end
+			});
+		}
+	}
+
+	// before combining our block and portal schedules, we need to resize the lunch
+
+	//create lunch block (edited as needed)
+	const newLunchBlock: ScheduleBlock = {
+		class: genericBlocks['lunch'],
+		start: moment(),
+		end: moment()
+	};
+
+	dayBlockSchedule.blocks.forEach(val => {
+		if (val.block === dayBlockSchedule.lunchBlock) {
+			const lunchBlock = val as blockSchedule.LunchBlockFormat;
+			const portalLunchBlock = portalSchedule.find(value => {
+				return value.class.block === val.block;
+			});
+
+			// if the lunch block overlaps with class, then use the other one, simple.
+			// if portalLunchBlock is null, user has free, so insert first lunch.
+
+			// a bit messy, but I have to convert everything to moment objects
+			lunchBlock.hswl.start = convertTimeStringToMoment(requestedDate, lunchBlock.hswl.start);
+			lunchBlock.hswl.end = convertTimeStringToMoment(requestedDate, lunchBlock.hswl.end);
+			lunchBlock.aemsh.start = convertTimeStringToMoment(
+				requestedDate,
+				lunchBlock.aemsh.start
+			);
+			lunchBlock.aemsh.end = convertTimeStringToMoment(requestedDate, lunchBlock.aemsh.end);
+
+			if (portalLunchBlock === undefined) {
+				const ishswlEarlier = (lunchBlock.hswl.start as Moment).isBefore(
+					lunchBlock.hswl.end
+				);
+
+				newLunchBlock.start = ishswlEarlier
+					? (lunchBlock.hswl.start as Moment)
+					: (lunchBlock.aemsh.start as Moment);
+				newLunchBlock.end = ishswlEarlier
+					? (lunchBlock.hswl.end as Moment)
+					: (lunchBlock.aemsh.end as Moment);
+				// break; <- turn this to a for of?
+			}
+
+			// details on time overlapping here: https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+			// slightly modified so that it can end/start at same times, but not overlap
+			const hswlOverlaps =
+				portalLunchBlock?.start.isSameOrAfter(lunchBlock.hswl.end) &&
+				portalLunchBlock?.end.isSameOrBefore(lunchBlock.hswl.start);
+			const aemshOverlaps =
+				portalLunchBlock?.start.isSameOrAfter(lunchBlock.aemsh.end) &&
+				portalLunchBlock?.end.isSameOrBefore(lunchBlock.aemsh.start);
+
+			console.log(lunchBlock);
+			console.log(portalLunchBlock);
+		}
+	});
+	// userSchedule.classes = ordineSchedule(userSchedule.classes, portalSchedule);
+
 	return { hasURL: true, schedule: userSchedule };
+}
+
+function convertTimeStringToMoment(date: moment.Moment, time: moment.Moment | string) {
+	if (time instanceof String) {
+		return date
+			.clone()
+			.hour(parseInt(time.split(':')[0], 10))
+			.minute(parseInt(time.split(':')[1], 10));
+	}
+	return time;
 }
 
 /**
@@ -290,12 +469,6 @@ function combineClassesSchedule(
 	// TODO: Is this still needed? Looks like something left behind after a refactor.
 	// noinspection JSUnusedAssignment
 	date = moment(date);
-	if (!Array.isArray(schedule)) {
-		schedule = [];
-	}
-	if (typeof blocks !== 'object') {
-		blocks = {};
-	}
 
 	// Loop through schedule
 	const combinedSchedule: ScheduleClasses = [];
@@ -331,6 +504,149 @@ function combineClassesSchedule(
 
 	JSON.stringify(combinedSchedule);
 	return combinedSchedule;
+}
+
+/**
+ * --------------------------------
+ * or·dine
+ * ôrdīn
+ * verb
+ *
+ * 1. To order and combine
+ *
+ * - Michael Gira.
+ * --------------------------------
+ * Combines the two classes and orders them properly.
+ * @param baseSchedule The existing classes.
+ * @param addClasses The new blocks to add. Will override base classes if there is a conflict.
+ * @returns A sorted array of classes.
+ */
+function ordineSchedule(
+	baseSchedule: ClassesOrBlocks,
+	addClasses: ClassesOrBlocks
+): ClassesOrBlocks {
+	if (!Array.isArray(baseSchedule)) {
+		baseSchedule = [];
+	}
+	if (!Array.isArray(addClasses)) {
+		addClasses = [];
+	}
+
+	// Add each class to the base schedule
+	for (const addClass of addClasses) {
+		const start = moment(addClass.start);
+		const end = moment(addClass.end);
+
+		// Keep track of conflicting indexes
+		const conflictIndexes: number[] = [];
+
+		// Move other (if any) events with conflicting times
+		for (let i = 0; i < baseSchedule.length; i++) {
+			const scheduleClass = baseSchedule[i];
+
+			const blockStart = moment(scheduleClass.start);
+			const blockEnd = moment(scheduleClass.end);
+			// Determine start/end times relative to the class we're currently trying to add
+			let startRelation = null;
+			if (start.isSame(blockStart)) {
+				startRelation = 'same start';
+			} else if (start.isSame(blockEnd)) {
+				startRelation = 'same end';
+			} else if (start.isBefore(blockStart)) {
+				startRelation = 'before';
+			} else if (start.isAfter(blockEnd)) {
+				startRelation = 'after';
+			} else if (start.isAfter(blockStart) && start.isBefore(blockEnd)) {
+				startRelation = 'inside';
+			}
+
+			let endRelation = null;
+			if (end.isSame(blockStart)) {
+				endRelation = 'same start';
+			} else if (end.isSame(blockEnd)) {
+				endRelation = 'same end';
+			} else if (end.isBefore(blockStart)) {
+				endRelation = 'before';
+			} else if (end.isAfter(blockEnd)) {
+				endRelation = 'after';
+			} else if (end.isAfter(blockStart) && end.isBefore(blockEnd)) {
+				endRelation = 'inside';
+			}
+
+			// If new event is totally unrelated to the block, just ignore
+			if (startRelation === 'same end' || startRelation === 'after') {
+				continue;
+			}
+			if (endRelation === 'same start' || endRelation === 'before') {
+				continue;
+			}
+
+			// If start is before or equal to block start
+			if (startRelation === 'before' || startRelation === 'same start') {
+				// If end is inside, we can still keep half of the block
+				if (endRelation === 'inside') {
+					baseSchedule[i].start = end.clone();
+				}
+
+				// If new class completely engulfs the block, delete
+				if (endRelation === 'same end' || endRelation === 'after') {
+					// Only push to array if index isn't already in array
+					if (!conflictIndexes.includes(i)) {
+						conflictIndexes.push(i);
+					}
+				}
+			}
+
+			// If end is inside the block
+			if (startRelation === 'inside') {
+				// If new event is inside block
+				if (endRelation === 'inside') {
+					// Split event into two
+					const newBlock = JSON.parse(JSON.stringify(scheduleClass));
+
+					// Set old block to beginning of next block
+					baseSchedule[i].end = start.clone();
+					// Set new block start where the next block left off
+					newBlock.start = end.clone();
+					// Also make sure end is a moment object because it goes through JSON.stringify
+					newBlock.end = moment(newBlock.end);
+
+					(baseSchedule as any[]).push(newBlock);
+				}
+
+				if (endRelation === 'same end' || endRelation === 'after') {
+					baseSchedule[i].end = start.clone();
+				}
+			}
+
+			// If same times, delete
+			if (startRelation === 'same start' && endRelation === 'same end') {
+				// Only push to array if index isn't already in array
+				if (!conflictIndexes.includes(i)) {
+					conflictIndexes.push(i);
+				}
+			}
+		}
+
+		// Delete all conflicting classes
+		conflictIndexes.sort();
+		let deleteOffset = 0;
+		for (const conflictIndex of conflictIndexes) {
+			const index = conflictIndex - deleteOffset++;
+			baseSchedule.splice(index, 1);
+		}
+
+		// After all other classes are accounted for, add this new class
+		(baseSchedule as any[]).push(addClass);
+	}
+
+	// Delete all classes that start and end at the same time, or end is before start
+	baseSchedule = (baseSchedule as any[]).filter(value => value.start.unix() < value.end.unix());
+
+	// Reorder schedule because of deleted classes
+	(baseSchedule as any[]).sort((a, b) => a.start - b.start);
+
+	return baseSchedule;
 }
 
 export interface FullSchedule {
