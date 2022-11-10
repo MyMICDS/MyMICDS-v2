3d2c4a5d2a86c629b6b2c91e698d8ca5d5d66a91
Sam Baumohl <28550422+sbaumohl@users.noreply.github.com>
2022-11-09 22:03:30 -0600

    Update user.ts

--- test/helpers/user.ts
+++ test/helpers/user.ts
@@ -8,7 +8,7 @@ export const testUser = {
 	password: 'test',
 	firstName: 'Test',
 	lastName: 'User',
-	gradYear: 2022,
+	gradYear: 2024,
 	confirmed: true,
 	registered: new Date(),
 	confirmationHash: 'asdf',
